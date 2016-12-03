119e69bc7049632b2a5b664c52406ea7d1965433
noahmilstein <casanoah@gmail.com>
2016-12-02 22:01:18 -0500

    add comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  # prune redundant routes and relevant methods within controllers
+  # possibly redundant
+  resources :comments
+
   devise_for :users
   resources :links do
     member do
       put "like", to: "links#upvote"
       put "dislike", to: "links#downvote"
     end
+    resources :comments
   end
 
   root 'links#index'
